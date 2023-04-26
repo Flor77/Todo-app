b10fcbbfa0d23a9b136fe74c65c3f196b4b61aa6
Flor77 <budaflorin@yahoo.com>
2023-04-26 20:43:56 +0300

    checkbox component modified

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,28 +1,28 @@
 import { useState, useEffect } from "react";
 
-const CheckBox = (props) => {
-  const [checked, setChecked] = useState(!!props.checked);
+const CheckBox = ({ checked = false, onChange }) => {
+  const [isChecked, setIsChecked] = useState(checked);
 
   const handleCheckboxChange = (e) => {
-    setChecked(e.target.checked);
+    setIsChecked(e.target.checked);
   };
 
   useEffect(() => {
-    if (props.onChange) {
-      props.onChange(checked);
+    if (onChange) {
+      onChange(isChecked);
     }
-  }, [checked]);
+  }, [isChecked]);
 
   return (
     <div className="round">
       <input
         type="checkbox"
-        value={checked}
-        checked={checked}
+        // value={isChecked}
+        checked={isChecked}
         onChange={handleCheckboxChange}
       />
       <label
-        className={`checkbox ${checked ? "checkbox--active" : ""}`}
+        className={`checkbox ${isChecked ? "checkbox--active" : ""}`}
         htmlFor="checkbox"
       ></label>
     </div>
