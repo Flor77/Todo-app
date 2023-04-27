ad6623a97e991830d3dc13f5291cae05e9ecd25a
Flor77 <budaflorin@yahoo.com>
2023-04-27 17:42:56 +0300

    memoizedOnChange funct

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 const CheckBox = ({ checked = false, onChange }) => {
   const [isChecked, setIsChecked] = useState(checked);
@@ -7,17 +7,19 @@ const CheckBox = ({ checked = false, onChange }) => {
     setIsChecked(e.target.checked);
   };
 
+  const memoizedOnChange = useCallback(onChange, [isChecked]);
+
   useEffect(() => {
-    if (onChange) {
-      onChange(isChecked);
+    if (memoizedOnChange) {
+      memoizedOnChange(isChecked);
     }
-  }, [isChecked]);
+  }, [isChecked, memoizedOnChange]);
 
   return (
     <div className="round">
       <input
         type="checkbox"
-        // value={isChecked}
+        value={isChecked}
         checked={isChecked}
         onChange={handleCheckboxChange}
       />
